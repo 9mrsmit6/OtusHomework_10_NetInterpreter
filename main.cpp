e5d59e9d913d9800adc3df37bcce33743a687596
mrsmit <9mrsmit6@gmail.com>
2023-05-03 13:10:29 +0300

    поигрался с boost asio

--- main.cpp
+++ main.cpp
@@ -1,9 +1,64 @@
 #include <iostream>
+#include <boost/asio.hpp>
+#include <string>
 
-using namespace std;
+boost::asio::ip::tcp::socket* sockPtr;
+std::string str{"                              "};
+
+char vax[100];
+
+void read_handler(const boost::system::error_code& error, std::size_t readed)
+{
+    if (!error)
+    {
+        std::cout<<str<<std::endl;
+        auto buf=boost::asio::buffer(str);
+        boost::asio::async_read(*sockPtr, buf, read_handler);
+
+    }
+    else
+    {
+        std::cout<<"Closed"<<std::endl;
+    }
+
+}
+
+void accept_handler(const boost::system::error_code& error)
+    {
+        if (!error)
+        {
+            // Accept succeeded.
+
+            std::cout<<"ACC"<<std::endl;
+
+            auto buf=boost::asio::buffer(str);
+            boost::asio::async_read(*sockPtr, buf, read_handler);
+        }
+    }
+
+
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    boost::asio::io_context io_context;
+
+    boost::asio::ip::tcp::acceptor ac{io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 9000)};
+    boost::asio::ip::tcp::socket socket{io_context};
+
+    sockPtr=&socket;
+
+    ac.async_accept(socket, accept_handler);
+
+    io_context.run();
+
+    while(true)
+    {
+
+    }
+
+
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
